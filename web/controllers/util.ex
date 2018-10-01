37262f6a3d145f5d6c80bf6c866f31709e47694d
Matt Tanous <mtanous22@gmail.com>
2018-10-01 19:26:30 -0400

    Initial work on viewing video (needs debugging)

--- web/controllers/util.ex
+++ web/controllers/util.ex
@@ -2,4 +2,12 @@ defmodule PhoenixVideoStream.Util do
   def build_video_path(video) do
     Application.get_env(:phoenix_video_stream, :uploads_dir) |> Path.join(video.path)
   end
+
+  def send_video(conn, headers, video) do
+    video_path = build_video_path(video)
+
+    conn
+    |> Plug.Conn.put_resp_header("content-type", video.content_type)
+    |> Plug.Conn.send_file(200, video_path)
+  end
 end
