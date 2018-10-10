311f09f126110c17fe79423fe14932067771b058
Matt Tanous <mtanous22@gmail.com>
2018-10-10 16:57:50 -0400

    Migrate to Phoenix 1.3 assets directory structure

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,7 @@ config :phoenix_video_stream, PhoenixVideoStream.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../", __DIR__)]]
+                    cd: Path.expand("../assets", __DIR__)]]
 
 
 # Watch static and templates for browser reloading.
