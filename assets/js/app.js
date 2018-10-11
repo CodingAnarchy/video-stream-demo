89f41af04f0c8f0791cdbf17f7191f28304fbf48
Matt Tanous <mtanous22@gmail.com>
2018-10-11 10:34:58 -0400

    CSS styling fixes for flash messages in Foundation

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,5 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+window.setTimeout(function() { $(".flash-alert").slideUp() }, 3000);
